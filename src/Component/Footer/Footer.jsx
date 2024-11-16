141eb43b3494650f976658cc5db0aabb4fefde35
kaifsk102 <kaifsk102@gmail.com>
2024-11-16 19:26:55 +0530

    commited

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -15,18 +15,19 @@ const Footer = () => {
         <div> 
         <h4 className="font-bold mb-2">Netflix</h4> 
         <ul> 
-          <li><a href="/marketing" className="hover:underline">Marketing</a></li> 
-          <li><a href="/analytics" className="hover:underline">Analytics</a></li> 
-          <li><a href="/automation" className="hover:underline">Automation</a></li> 
-          <li><a href="/commerce" className="hover:underline">Commerce</a></li> 
-          <li><a href="/insights" className="hover:underline">Insights</a></li> 
+          <li><a href="/FAQ" className="hover:underline">FAQ</a></li> 
+          <li><a href="/Investor-Relations" className="hover:underline">Investor Relations</a></li> 
+          <li><a href="/Privacy" className="hover:underline">Privacy</a></li> 
+          <li><a href="/Speed-Test" className="hover:underline">Speed Test</a></li> 
+          <li><a href="/Legal-Notice" className="hover:underline">Legal Notice</a></li> 
         </ul> 
         </div> 
         <div> 
           <h4 className="font-bold mb-2">Support</h4> 
           <ul>
-            <li><a href="/submit-ticket" className="hover:underline">Submit ticket</a></li> 
-            <li><a href="/documentation" className="hover:underline">Documentation</a></li> 
+            <li><a href="/Help-Center" className="hover:underline">Help Center</a></li> 
+            <li><a href="/Ways-to-Watch" className="hover:underline">Ways to Watch</a></li> 
+            <li><a href="/Natflix-Orginal" className="hover:underline">Netflix Orginal</a></li> 
             <li><a href="/guides" className="hover:underline">Guides</a></li> 
           </ul> 
         </div> 
@@ -36,7 +37,7 @@ const Footer = () => {
               <li><a href="/about" className="hover:underline">About</a></li> 
               <li><a href="/blog" className="hover:underline">Blog</a></li> 
               <li><a href="/jobs" className="hover:underline">Jobs</a></li> 
-              <li><a href="/press" className="hover:underline">Press</a></li> 
+              <li><a href="/Account" className="hover:underline">Account</a></li> 
             </ul> 
         </div> 
       </div> 
